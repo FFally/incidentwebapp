5fe98ef270d16e7ee7bba26d33685cc755e63f41
FF <a01251567@univie.ac.at>
2021-02-08 18:24:29 +0100

    First Draft with HTML and NavigationBar

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Felix'}
+    posts = [
+        {
+            'author': {'username': 'Q1'},
+            'body': 'Has measurement A been applied?'
+        },
+        {
+            'author': {'username': 'Q2'},
+            'body': 'Has measurement B been applied?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
