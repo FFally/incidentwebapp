2950e4f7460cd57520bba82b65390d7553ae183e
FF <a01251567@univie.ac.at>
2021-02-10 16:48:46 +0100

    HTML File Reader working, installed beautifulsoup

---
+++ measuresapi.py
@@ -0,0 +1,37 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+# HTML File-Reader Approach
+with open("measures.html", "r", encoding='utf-8') as f:
+    text= f.read()
+soup = BeautifulSoup(text,'html.parser')
+
+#print all Measurement Headlines
+'''
+pAll = soup.find_all('h3')
+print(pAll)
+'''
+#print content of id 738
+#find by id
+cid = soup.find(id="topic_738")
+#print only text fro chapter
+print(cid.get_text())
+
+
+
+
+
+# URL Request Approach
+'''
+content = urllib.request.urlopen('https://www.sicherheitshandbuch.gv.at/#738')
+read_content = content.read()
+soup = BeautifulSoup(read_content,'html.parser')
+
+# print all Tags
+for tag in soup.find_all(True):
+    print(tag.name)
+
+'''
+
+
+
--- mitre_api.py
+++ mitre_api.py
@@ -5,6 +5,22 @@ from taxii2client.v20 import Server # only specify v20 if your installed version
 
 server = Server("https://cti-taxii.mitre.org/taxii/")
 api_root = server.api_roots[0]
+'''
 # Print name and ID of all ATT&CK domains available as collections
 for collection in api_root.collections:
     print(collection.title.ljust(20) + collection.id)
+'''
+collections = {
+    "enterprise_attack": "95ecc380-afe9-11e4-9b6c-751b66dd541e",
+    "mobile_attack": "2f669986-b40b-4423-b720-4396ca6a462b",
+    "ics-attack": "02c3ef24-9cd4-48f3-a99f-b74ce24f1d34"
+}
+
+
+collection = Collection(f"https://cti-taxii.mitre.org/stix/collections/{collections['enterprise_attack']}/")
+src = TAXIICollectionSource(collection)
+phish = src.query([ Filter("external_references.external_id", "=", "T1566") ])[0]
+
+print(phish['name']) #print name of technique phishing
+print(phish['description']) #print description of technique phishing 
+
